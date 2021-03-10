b168dc2ab016345ab6f1cdb42b205b7095ecb354
sekarpov <sergo8ck@gmail.com>
2021-03-10 21:04:28 +0300

    Vue Router

--- resources/js/components/Example2.vue
+++ resources/js/components/Example2.vue
@@ -3,9 +3,9 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">Example Component</div>
+          <div class="card-header">Second Component</div>
 
-          <div class="card-body">I'm an example component.</div>
+          <div class="card-body">I'm an second component.</div>
         </div>
       </div>
     </div>
