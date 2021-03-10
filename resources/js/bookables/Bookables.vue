ba81ae67107f2f0dfea14e560e0c755e7eeb3a66
sekarpov <sergo8ck@gmail.com>
2021-03-10 22:37:19 +0300

    Api frontend

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -4,15 +4,11 @@
         <div v-else>
             <div class="row mb-4" v-for="row in rows" :key="'row' + row">
                 <div
-                    class="col"
+                    class="col d-flex align-items-stretch"
                     v-for="(bookable, column) in bookablesInRow(row)"
                     :key="'row' + row + column"
                 >
-                    <bookable-list-item
-                        :item-title="bookable.title"
-                        :item-content="bookable.content"
-                        :price="1000"
-                    ></bookable-list-item>
+                    <bookable-list-item v-bind="bookable"></bookable-list-item>
                 </div>
 
                 <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder' + row + p"></div>
@@ -52,40 +48,21 @@ export default {
     },
     created() {
         this.loading = true;
-        setTimeout(() => {
-            this.bookables = [
-                {
-                    id: 1,
-                    title: "Cheap Villa !!!",
-                    content: "A very cheap villa"
-                },
-                {
-                    title: "Cheap Villa 2",
-                    content: "A very cheap villa 2"
-                },
-                {
-                    title: "Cheap Villa 2",
-                    content: "A very cheap villa 2"
-                },
-                {
-                    title: "Cheap Villa 2",
-                    content: "A very cheap villa 2"
-                },
-                {
-                    title: "Cheap Villa 2",
-                    content: "A very cheap villa 2"
-                },
-                {
-                    title: "Cheap Villa 2",
-                    content: "A very cheap villa 2"
-                },
-                {
-                    title: "Cheap Villa 2",
-                    content: "A very cheap villa 2"
-                }
-            ];
+
+        const p = new Promise((resolve, reject) => {
+            console.log(resolve);
+            console.log(reject);
+            setTimeout(() => resolve("Hello"), 3000);
+        })
+            .then(result => "Hello again " + result)
+            .then(result => console.log(result))
+            .catch(result => console.log(`Error ${result}`));
+        console.log(p);
+
+        const request = axios.get("/api/bookables").then(response => {
+            this.bookables = response.data;
             this.loading = false;
-        }, 2000);
+        });
     }
 };
 </script>
