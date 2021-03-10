69848116076fd9e78bbe14178bb31614c78c0837
sekarpov <sergo8ck@gmail.com>
2021-03-11 00:45:49 +0300

    StarRating

--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -7,7 +7,9 @@
             <div class="border-bottom d-none d-md-block" v-for="(review, index) in reviews" :key="index">
                 <div class="row pt-4">
                     <div class="col-md-6">Piotr Jura</div>
-                    <div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                        <star-rating :value="review.rating" class="fa-lg"></star-rating>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">{{ review.created_at | fromNow }}</div>
---
+++ resources/js/review/Review.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+            <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+        </div>
+        <div class="form-group">
+            <label for="content" class="text-muted">Describe your expirience with</label>
+            <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+        </div>
+
+        <button class="btn btn-lg btn-primary btn-block">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            review: {
+                rating: 5,
+                content: null
+            }
+        };
+    },
+    created() {
+        // 1. If review already exists (in reviews table by id)
+        // 2. Fetch a booking by a review key
+        // 3. Store the review
+    }
+};
+</script>
